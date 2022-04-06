082d8606add0b8e00c49f1b127b5cb692adfd26d
Simon Blurton <sblurton@hotmail.co.uk>
2022-04-05 21:41:18 -0500

    new feature: example working.

--- example/index.tsx
+++ example/index.tsx
@@ -1,12 +1,34 @@
-import 'react-app-polyfill/ie11';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { Thing } from '../.';
+import useArray from '../src';
 
 const App = () => {
+  const { array, set, clear, filter, push, remove, update } = useArray([
+    1,
+    2,
+    3,
+    4,
+    5,
+    6,
+    7,
+  ]);
   return (
     <div>
-      <Thing />
+      <h2>{array.length > 0 ? array.join(', ') : 'there is no array.'}</h2>
+      <button onClick={() => push(7)}>add 7</button>
+      <button onClick={() => update(1, 9)}>change second element to 9</button>
+      <button onClick={() => remove(1)}>remove second element</button>
+      <button onClick={() => filter((n: number) => n < 3)}>
+        filter out 4 or higher
+      </button>
+      <button onClick={clear}>clear</button>
+      <button
+        onClick={() => {
+          set([7, 6, 5, 4, 3, 2, 1]);
+        }}
+      >
+        set to 7, 6, 5, 4, 3, 2, 1
+      </button>
     </div>
   );
 };
